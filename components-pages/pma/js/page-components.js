e269101dd0bf2eb19f8953fe78e572ae7272d861
Raymond R-Mcnaught <raymond.richards-mcnaught@grey.com>
2016-08-02 00:42:58 -0400

    built object-audio-component. Still need work but it's resizable

--- components-pages/pma/js/page-components.js
+++ components-pages/pma/js/page-components.js
@@ -7,6 +7,7 @@ $(function(){
     prevButton, 
     nextButton,
     thumbnailClickOn =  Boolean,
+    audioOn = Boolean,
     imgArr = [];
 
   ////////INCLUDES for component html files
@@ -118,6 +119,67 @@ $(function(){
       $('.carousel-component-list ul').animate({'height': liHeight/2}, 200, 'swing');
 
   }); 
+
+
+  ///// AUDIO
+  var audio1 = new Audio('../pma/audio/Classical Indian Music.mp3');
+    
+  function audioPlayer(trackVar, id, currTimeText, proGressTime, pBar) {
+      var id,
+        trackVar,
+        currTimeText,
+        proGressTime,
+        pBar,
+        duration = trackVar.duration;
+
+      //toggle play button
+      $(id).click(function(){
+          if(trackVar.paused){
+              trackVar.play();
+              console.log('Track ',trackVar, ' on ', '  audioOn = ', audioOn);
+            }else{
+              trackVar.pause();
+              console.log('Track ',trackVar, ' mute', '  audioOn = ', audioOn);
+          };
+          console.log( trackVar, ' ',trackVar.duration );
+      });
+
+      //PROGRESS BAR GET idEO PROGRESSION CODE - http://www.developphp.com/ideo/JavaScript/ideo-Duration-and-Current-Play-Time-Programming-Tutorial
+      trackVar.addEventListener('timeupdate', seekTimerUpdate(currTimeText, proGressTime, pBar), false);
+      function seekTimerUpdate(currTimeText, proGressTime, pBar){
+        var time = trackVar.currentTime * (100/trackVar.duration),
+        curmins = Math.floor(trackVar.currentTime /60),
+        cursecs = Math.floor(trackVar.currentTime - curmins * 60),
+        durmins = Math.floor(trackVar.duration /60),
+        dursecs = Math.floor(trackVar.duration - durmins * 60);
+
+        if(cursecs < 10){ cursecs = "0" + cursecs; };
+        if(dursecs < 10){ dursecs = "0" + dursecs; };
+        if(curmins < 10){ curmins = "0" + curmins; };
+        if(durmins < 10){ durmins = "0" + durmins; };
+
+        $(currTimeText).text(curmins+":"+cursecs);
+        $(proGressTime).text(durmins+":"+dursecs);
+
+
+        //$(pBar).width(time + '%');
+        curtime = parseInt(trackVar.currentTime, 10);
+        $(pBar).attr("value", curtime);
+
+        console.log('Progress Bar width ',$(pBar).width(), ' ', $(pBar));
+        console.log('currTimeText ',(curmins+":"+cursecs), ' proGressTime ', (durmins+":"+dursecs));
+
+      }
+  };
+  // rRun audio
+  audioPlayer(audio1, '#playButton', '#currTime_box','#progress_box','#progressBar');
+
+  /*audio1.play(); - This will play the music.
+  audio1.pause(); - This will stop the music.
+  audio1.duration; - Returns the length of the music track.
+  audio1.currentTime = 0; - This will rewind the audio to the beginning.
+  audio1.loop = true; - This will make the audio track loop.
+  audio1.muted = true; - This will mute the track*/
 
 
   ///// RESIZE FUNCTION
